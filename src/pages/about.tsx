28c5a27dd208efd3df35c668af39e4e0b00dd47b
Clement Seconde-Kynnersely <c.sec.kyn@gmail.com>
2022-12-03 00:25:10 +0000

    (feat) adding html and styles to contact form

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -6,7 +6,7 @@ import { StaticImage } from 'gatsby-plugin-image';
 const AboutPage = () => {
   return (
     <Layout pageTitle="About Me">
-      <p>Hi there! I'm the proud creator of this site.</p>
+      <p>Hi there! We're the proud creators of this site.</p>
       <StaticImage
         alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
         src="https://images.unsplash.com/photo-1548199973-03cce0bbc87b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1769&q=80"
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Layout from '../components/layout';
 import { StaticImage } from 'gatsby-plugin-image';
 import Seo from '../components/seo';
-import * as styles from '../styles/pagesIndex.module.scss';
+import * as styles from '../styles/contact.module.scss';
 
 const IndexPage = () => {
   return (
@@ -13,24 +13,40 @@ const IndexPage = () => {
           alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
           src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
         />
-        <form action="post">
-          <label htmlFor="input">
-            <p>name</p>
-            <input type="text" />
-          </label>
-          <label htmlFor="input">
-            <p>email</p>
-            <input type="email" />
-          </label>
-          <label htmlFor="input">
-            <p>message</p>
+        <form action="SEND" method="POST" className={styles.form}>
+          <div className={styles.labelAndInputContainer}>
+            <label className={styles.label} htmlFor="Name">
+              Name
+            </label>
+            <input className={styles.input} type="text" name="Name" required />
+            {/* </div> */}
+            {/* <div className={styles.labelAndInputContainer}> */}
+            <label className={styles.label} htmlFor="Email">
+              Email
+            </label>
+            <input
+              className={styles.input}
+              type="email"
+              name="Email"
+              required
+            />
+            {/* </div> */}
+            {/* <div className={styles.labelAndInputContainer}> */}
+            <label className={styles.label} htmlFor="Message">
+              Message
+            </label>
             <textarea
-              name="message"
+              className={styles.input}
+              name="Message"
               id="nessage"
               cols={30}
               rows={10}
-            ></textarea>
-          </label>
+              required
+            />
+            <div className={styles.submit}>
+              <input type="submit" value="Send" />
+            </div>
+          </div>
         </form>
       </Layout>
     </div>
